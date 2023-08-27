46c9e4901ba5d68c4971b263c8e54b5d2779f4e8
vsalfafara <alfafara.vm@gmail.com>
2023-08-27 12:29:28 +0800

    Removed netlify configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 
-const viteEnv = {};
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
-  }
-});
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
