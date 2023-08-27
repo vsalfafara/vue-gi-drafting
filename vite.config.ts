7364d00823c29256057228b59ce31019fa42100d
vsalfafara <alfafara.vm@gmail.com>
2023-08-27 12:21:19 +0800

    Added netlify env solution in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 
-// https://vitejs.dev/config/
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
